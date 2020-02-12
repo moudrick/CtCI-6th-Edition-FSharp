ad04065d5d6dd5e115acdb1cd5929c0517d0ef2a
moudrick <moudrick@gmail.com>
2020-02-13 00:18:53 +0200

    1.6 Sln & Tests + Bad + Better

---
+++ Ch 01. Arrays and Strings/Q1 06 String Compression.fs
@@ -0,0 +1,94 @@
+﻿namespace Ch_01._Arrays_and_Strings.``Q1 06 - String Compression``
+
+module Sln =
+
+    open System.Text
+
+    module Bad =
+        let compress str =
+            seq {
+                let mutable compressedString = ""
+                let mutable countConsecutive = 0
+                let length = str |> String.length
+
+                for i in 0 .. length - 1 do
+                    countConsecutive <- countConsecutive + 1
+
+                    (* If next character is different than current, append this char to result. *)
+                    if (i + 1 >= length || str.[i] <> str.[i + 1]) 
+                    then
+                        compressedString <- compressedString + str.[i].ToString() + countConsecutive.ToString()
+                        countConsecutive <- 0
+
+                yield if (compressedString |> String.length < length)
+                    then compressedString 
+                    else str
+            } |> Seq.head
+
+    let compress str =
+
+        let compressed = new StringBuilder()
+        let mutable countConsecutive = 0
+        let length = str |> String.length
+
+        for i in 0 .. length - 1 do
+            countConsecutive <- countConsecutive + 1
+
+            if (i + 1 >= length || str.[i] <> str.[i + 1]) 
+            then
+                compressed.Append(str.[i]) |> ignore
+                compressed.Append(countConsecutive) |> ignore
+                countConsecutive <- 0
+        if (compressed.Length < length)
+            then compressed.ToString() 
+            else str
+
+
+    module Better =
+        let compress (str : string) : string =
+            seq {
+                let countCompression str =
+                    let mutable compressedLength = 0
+                    let mutable countConsecutive = 0
+                    let length = str |> String.length
+
+                    for i in 0 .. length - 1 do
+                        countConsecutive <- countConsecutive + 1
+
+                        (* If next character is different than current, append this char to result.*)
+                        if (i + 1 >= length || str.[i] <> str.[i + 1])
+                        then
+                            compressedLength <- compressedLength + 1 + countConsecutive.ToString().Length
+                            countConsecutive <- 0
+                    compressedLength
+
+                let length = str |> String.length
+                let finalLength = str |> countCompression
+                if (finalLength >= length) then yield str
+
+                let compressed = new StringBuilder(finalLength) // initialize capacity
+                let mutable countConsecutive = 0
+
+                for i in 0 .. length - 1 do
+                    countConsecutive <- countConsecutive + 1
+
+                    (* If next character is different than current, append this char to result.*)
+                    if (i + 1 >= length || str.[i] <> str.[i + 1])
+                    then
+                        compressed.Append(str.[i]) |> ignore
+                        compressed.Append(countConsecutive) |> ignore
+                        countConsecutive <- 0
+                yield compressed.ToString()
+            } |> Seq.head
+
+type Question() =
+    inherit ctci.Contracts.Question()
+
+    override this.DemoRun() =
+
+       let original = "abbccccccde";
+
+       printfn "Original : '%s'" original
+       printfn "Compressed Bad: '%s'" (original |> Sln.Bad.compress) 
+       printfn "Compressed: '%s'" (original |> Sln.compress)
+       printfn "Compressed Better: '%s'" (original |> Sln.Better.compress)
---
+++ ctci.Properties/Ch_01._Arrays_and_Strings.``Q1 06 - String Compression``.fs
@@ -0,0 +1,44 @@
+﻿module ctci.Properties.
+    Ch_01._Arrays_and_Strings.
+    ``Q1 06 - String Compression``
+
+open FsCheck
+open FsCheck.Xunit
+
+open Ch_01._Arrays_and_Strings.``Q1 06 - String Compression``
+
+type Case = { original : string; compressed : string; name : string }
+    with static member get o c n = { original = o; compressed = c; name = n }
+
+let cases = seq [
+    Case.get "" "" "Empty string"
+    Case.get "a" "a" "One symbol"
+    Case.get "aa" "aa" "Two same symbols"
+    Case.get "aabcccccaaa" "a2b1c5a3" "General case"
+    Case.get "compressme" "compressme" "WhenOriginalIsShorter: compressme is shorter than c1o1m1p1r1e1s2m1e1"
+    Case.get "aaab" "aaab" "WhenCompressedIsNotShorterButWeDontKnowThatUntilTheEnd: a3b1 is not shorter than aaab"
+    Case.get "aabccccca" "a2b1c5a1" "One at end"
+    Case.get "abbccccccde" "a1b2c6d1e1" "One at end 2"
+]
+
+type CasesGenArb() = 
+    static member Arb() = 
+        cases |> Gen.elements |> Arb.fromGen
+
+[< Property(Verbose = true, Arbitrary = [| typeof<CasesGenArb> |]) >]
+let ``Sln.Bad.compress works properly``
+    (case : Case) =
+
+    Sln.Bad.compress case.original = case.compressed
+
+[< Property(Verbose = true, Arbitrary = [| typeof<CasesGenArb> |]) >]
+let ``Sln.compress works properly``
+    (case : Case) =
+
+    Sln.compress case.original = case.compressed
+
+[< Property(Verbose = true, Arbitrary = [| typeof<CasesGenArb> |]) >]
+let ``Sln.Better.compress works properly``
+    (case : Case) =
+
+    Sln.Better.compress case.original = case.compressed
